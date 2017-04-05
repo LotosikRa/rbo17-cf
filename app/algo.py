d1509e6009dcb4be0fbee98f47e8b4fe313c36b5
Illia Ananich <illia.ananich@gmail.com>
2017-04-05 21:33:48 +0300

    improve docstring for `calculate`

--- app/algo.py
+++ app/algo.py
@@ -1,13 +1,14 @@
 """ This module contains algorithms. """
 
 
-def calculate(a, b, t, k):
+def calculate(x_long, y_long, total_dots, goal, input_array):
     """
-
-    :param a: long of side A (x axis)
-    :param b: long of side B (y axis)
-    :param t: number of checkers
-    :param k: minimum of checkers on the one line
-    :return: coins
+    :param input_array: array with `y_long` arrays each one with `x_long` boolean integer (e.g. 0, 1)
+    :param x_long: long of side A (x axis)
+    :param y_long: long of side B (y axis)
+    :param total_dots: number of checkers
+    :param goal: minimum of checkers on the one line
+    :return: points
     """
-    pass
+    points = 0
+    return points
