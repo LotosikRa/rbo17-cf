09aba5b70770e6f28e955bc4f963681357d6294a
Ilia Ananich <illia.ananich@gmail.com>
2017-04-05 18:02:15 +0300

    define Python modules

---
+++ app/algo.py
@@ -0,0 +1,13 @@
+""" This module contains algorithms. """
+
+
+def calculate(a, b, t, k):
+    """
+
+    :param a: long of side A (x axis)
+    :param b: long of side B (y axis)
+    :param t: number of checkers
+    :param k: minimum of checkers on the one line
+    :return: coins
+    """
+    pass
---
+++ cli.py
@@ -0,0 +1,10 @@
+""" This module runs algorithm with CLI. """
+from app import calculate
+
+
+def main():
+    print('Hello World!')
+
+
+if __name__ == '__main__':
+    main()
