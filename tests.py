2505bf68468cbd7d622d30a69481a1cb55e61157
Illia Ananich <illia.ananich@gmail.com>
2017-06-22 12:42:45 +0300

    upgrade tests

--- tests.py
+++ tests.py
@@ -6,14 +6,14 @@ try:
 except ImportError:
     pass
 else:
-    @pytest.mark.parametrize('array, expected', [(psta(s), e) for s, e in e.samples],
+    @pytest.mark.parametrize('array, expected, goal', [(psta(s), e, g) for s, e, g in e.samples],
                              ids=[str(i) for i in range(1, len(e.samples) + 1)])
-    def test_sample(array: list, expected: int, benchmark):
-        assert benchmark(algo.calculate, array, goal=4) == expected
+    def test_sample(array: list, expected: int, goal: int, benchmark):
+        assert benchmark(algo.calculate, array, goal) == expected
 finally:
     if __name__ == '__main__':
-        assert algo.calculate(psta(e.sample1)) == 6
-        assert algo.calculate(psta(e.sample2)) == 4
-        assert algo.calculate(psta(e.sample3)) == 4
-        assert algo.calculate(psta(e.sample4)) == 10
+        assert algo.calculate(psta(e.sample1), 4) == 6
+        assert algo.calculate(psta(e.sample2), 4) == 4
+        assert algo.calculate(psta(e.sample3), 4) == 4
+        assert algo.calculate(psta(e.sample4), 4) == 10
         print('Tests completed.')
