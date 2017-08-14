a26ffa19c31f9ee3f5c1e18d3755fd20016adabc
Illia Ananich <illia.ananich@gmail.com>
2017-08-14 14:14:47 +0300

    new example

--- app/examples.py
+++ app/examples.py
@@ -4,8 +4,8 @@ from settings import COLUMNS, ROWS
 
 
 # constants
-exist = 1
-not_exist = 0
+exist = 'X'
+not_exist = ' '
 
 
 # helper function
@@ -17,7 +17,7 @@ def get_array(tuples_array):
         for y in range(COLUMNS):
             example[x].append(not_exist)
 
-    for y, x in tuples_array:  # for better graphical reproduction
+    for x, y in tuples_array:  # for better graphical reproduction
         example[x][y] = exist
 
     return example
@@ -53,19 +53,30 @@ sample4 = '0,0 1,0 2,0 3,0 4,0 0,1 1,1 3,1 4,1 0,2 2,2 4,2' \
 . .   . .
 . . . . .
 """
+sample5 = '0,0 1,0 3,0 4,0 1,2 2,2 3,2 5,2 1,4 2,4 3,4 0,6 1,6 3,6 4,6'
+""" 4-9
+XX XX
+
+ XXX X
+
+ XXX
+
+XX XX
+"""
 
 
 samples = [(sample1, 6, 4),
            (sample2, 4, 4),
            (sample3, 4, 4),
-           (sample4, 10, 4)]
+           (sample4, 10, 4),
+           (sample5, 9, 4), ]
 
 
 if __name__ == '__main__':
     # prints all existed examples
-    for e, _ in samples:
+    for e, *_ in samples:
         print('\tStart:')
         array = get_array(parse_string_to_array(e))
         for i in range(len(array)):
-            print(array[i])
+            print(''.join(array[i]))
         print('\t:End')
